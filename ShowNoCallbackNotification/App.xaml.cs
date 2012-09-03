8920bfd8f04a9882ec9319481e51688a895220dc
francois <francois@a65e54d1-7fdc-8e48-b40c-108a93a58451>
2012-09-03 20:19:33 +0000

    git-svn-id: svn://localhost/shownocallbacknotification/trunk@2 a65e54d1-7fdc-8e48-b40c-108a93a58451

---
+++ ShowNoCallbackNotification/App.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using SharedClasses;
+
+namespace ShowNoCallbackNotification
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		int _notifcount;
+		int NotificationCount
+		{
+			get { return _notifcount; }
+			set
+			{
+				_notifcount = value;
+				if (_notifcount == 0)
+					Environment.Exit(0);
+				//Exits if count is zero, app will just start again when needed
+			}
+		}
+
+		const int cDefaultTimeoutSeconds = 3;
+		private void ShowNotificationFromCommandlineArgs(string[] args)
+		{
+			if (args.Length < 2)//Does not have at least a title
+			{
+				//args = new string[] { "", "Title", "Message", "Warning", "20" };
+				if (NotificationCount == 0)
+					Environment.Exit(0);
+				return;
+			}
+
+			//First arg is exe path
+			//Format: thisapp.exe "Title" "Message" NotificationType SecondsToShow
+			//use -99 SecondsToShow to show forever
+			string title = args.Length > 1 ? args[1] : "[NOCOMMANDLINETITLE]";
+			string message = args.Length > 2 ? args[2] : "[NOCOMMANDLINEMESSAGE]";
+
+			string notiftypeStr = args.Length > 3 ? args[3] : "";
+			ShowNoCallbackNotificationInterop.NotificationTypes notifType;
+			if (!Enum.TryParse<ShowNoCallbackNotificationInterop.NotificationTypes>(notiftypeStr, true, out notifType))
+				notifType = ShowNoCallbackNotificationInterop.NotificationTypes.Subtle;
+
+			bool wasTimeoutMinus99forever = args.Length > 4 && args[4].Equals("-99");
+			string timeoutSecondsStr = args.Length > 4 ? args[4] : cDefaultTimeoutSeconds.ToString();
+			int timeoutSeconds;
+			if (!int.TryParse(timeoutSecondsStr, out timeoutSeconds)
+				|| timeoutSeconds < 0)
+				timeoutSeconds = cDefaultTimeoutSeconds;
+
+			
+			NotificationCount++;
+			TimeSpan? tmpTimeout = null;
+			if (!wasTimeoutMinus99forever)
+				tmpTimeout = TimeSpan.FromSeconds(timeoutSeconds);
+			WpfNotificationWindow.ShowNotification(
+				title: title,
+				message: message,
+				notificationType: notifType,
+				timeout: tmpTimeout,//If was -99 it will show forever
+				onCloseCallback_WasClickedToCallback: (o, wasclickedon) => { NotificationCount--; });
+		}
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			//base.OnStartup(e);
+			//MainWindow not used at all
+			SingleInstanceApplicationManager<MainWindow>.CheckIfAlreadyRunningElseCreateNew(
+				(evtargs, win) =>
+				{
+					//Second instances (what to do first first instance window)
+					ShowNotificationFromCommandlineArgs(evtargs.CommandLineArgs);
+				},
+				(args, win) =>
+				{
+					//First instance
+					SharedClasses.AutoUpdating.CheckForUpdates(
+						//SharedClasses.AutoUpdatingForm.CheckForUpdates(
+						//exitApplicationAction: () => Dispatcher.Invoke((Action)delegate { this.Shutdown(); }),
+						ActionIfUptoDate_Versionstring: (versionstring) => WpfNotificationWindow.SetCurrentVersionDisplayed(versionstring));
+
+					ShowNotificationFromCommandlineArgs(args);
+				});
+		}
+	}
+}
